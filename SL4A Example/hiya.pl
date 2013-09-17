4d6e8b083b21217e5342865739155fc22995bb6b
Michael D. Stemle, Jr. <themanchicken@gmail.com>
2013-09-17 15:06:28 -0500

    Update hiya.pl
    Cleaned it up a little bit, removed some debug code.

--- SL4A Example/hiya.pl
+++ SL4A Example/hiya.pl
@@ -2,107 +2,102 @@
 #
 
 use strict;use warnings;
-use Data::Dumper;
 
 {
-	package MyUiLoop;
+    package MyUiLoop;
 
-    use Data::Dumper;
-	
-	sub new {
-		my ($pkg, %opts) = @_;
-		
-		my $self = {%opts};
-		
-		$self->{_kill_loop} = 0;
-		$self->{_event_hooks} = {};
-		
-		return bless($self, $pkg);
-	}
-	
-	# Callbacks for hooks have arguments:
-	# - MyUiLoop Instance (the caller)
-	# - $droid An instance of Android
-	# - $event The event data
-	# - $data The extra data that was registered with the hook
-	
-	sub set_hook {
-		my ($self, $event_type, $id, $callback, $data) = @_;
-		
-		$self->{_event_hooks}->{$event_type}->{$id} = {
-			callback => $callback,
-			data     => $data,
-		};
-		
-		return $self;
-	}
-	
-	sub clear_hook {
-		my ($self, $event_type, $id) = @_;
-		
-		delete $self->{_event_hooks}->{$event_type}->{$id};
-		
-		return $self;
-	}
-	
-	sub _call_hook {
-		my ($self, $droid, $event) = @_;
-		
-		my $event_type = $event->{result}->{name};
-		my $id = $event->{result}->{data}->{id};
-		
-		if (!exists $self->{_event_hooks}->{$event_type} ||
-		    !exists $self->{_event_hooks}->{$event_type}->{$id}) {
-			main::double_print($droid, "No hook for '$event_type' and id '$id'");
-			return;
-		}
-		my $hook = $self->{_event_hooks}->{$event_type}->{$id};
-		
-		if (!defined($hook) || !ref($hook)) {
-			print "Bad hook!\n";
-			return;
-		}
-		
-		my $callback = $hook->{callback};
-		my $data = $hook->{data};
-		
-		$callback->($self, $droid, $event, $data);
-		
-		return;
-	}
-	
-	sub main_loop {
-		my ($self, $droid) = @_;
-		
-		while ($self->{_kill_loop} == 0) {
-			my $event = $droid->eventWait();
-			
-			print Dumper($event);
-			$self->_call_hook($droid, $event);
-		}
-		
-		# Yes, we keep going.
-	}
-	
-	sub exit_main_loop {
-		my ($self) = @_;
-		
-		$self->{_kill_loop} = 1;
-		
-		return $self;
-	}
+    sub new {
+        my ($pkg, %opts) = @_;
+        
+        my $self = {%opts};
+        
+        $self->{_kill_loop} = 0;
+        $self->{_event_hooks} = {};
+        
+        return bless($self, $pkg);
+    }
+    
+    # Callbacks for hooks have arguments:
+    # - MyUiLoop Instance (the caller)
+    # - $droid An instance of Android
+    # - $event The event data
+    # - $data The extra data that was registered with the hook
+    
+    sub set_hook {
+        my ($self, $event_type, $id, $callback, $data) = @_;
+        
+        $self->{_event_hooks}->{$event_type}->{$id} = {
+            callback => $callback,
+            data     => $data,
+        };
+        
+        return $self;
+    }
+    
+    sub clear_hook {
+        my ($self, $event_type, $id) = @_;
+        
+        delete $self->{_event_hooks}->{$event_type}->{$id};
+        
+        return $self;
+    }
+    
+    sub _call_hook {
+        my ($self, $droid, $event) = @_;
+        
+        my $event_type = $event->{result}->{name};
+        my $id = $event->{result}->{data}->{id};
+        
+        if (!exists $self->{_event_hooks}->{$event_type} ||
+            !exists $self->{_event_hooks}->{$event_type}->{$id}) {
+            main::double_print($droid, "No hook for '$event_type' and id '$id'");
+            return;
+        }
+        my $hook = $self->{_event_hooks}->{$event_type}->{$id};
+        
+        if (!defined($hook) || !ref($hook)) {
+            print "Bad hook!\n";
+            return;
+        }
+        
+        my $callback = $hook->{callback};
+        my $data = $hook->{data};
+        
+        $callback->($self, $droid, $event, $data);
+        
+        return;
+    }
+    
+    sub main_loop {
+        my ($self, $droid) = @_;
+        
+        while ($self->{_kill_loop} == 0) {
+            my $event = $droid->eventWait();
+            
+            $self->_call_hook($droid, $event);
+        }
+        
+        # Yes, we keep going.
+    }
+    
+    sub exit_main_loop {
+        my ($self) = @_;
+        
+        $self->{_kill_loop} = 1;
+        
+        return $self;
+    }
 }
 
 use Android;
-#use IO::File; -- BROKEN IN SL4A!
 
 sub double_print {
-	my ($droid, $msg) = @_;
+    my ($droid, $msg) = @_;
 
     if (defined($droid) && ref($droid)) {
-        $droid->log($msg);    	
-    }	
-	print "$msg\n";
+        $droid->log($msg);        
+    }    
+    print "$msg\n";
 }
 
 our $single_read = 1024;
@@ -115,7 +110,7 @@ open(my $uiIn, "<", $file_name) || die("Failed to open file '$file_name' for rea
 my $uiXml = "";
 
 while (read($uiIn, my $data, $single_read) != 0) {
-	$uiXml .= $data;
+    $uiXml .= $data;
 }
 double_print(undef, "I have UI XML: $uiXml");
 close($uiIn);
@@ -137,7 +132,7 @@ $businessEnd->set_hook(
         $caller->exit_main_loop();
         main::double_print($droid, "We got a request to exit!");
         
-        return;    	
+        return;        
     },
     undef
 );
@@ -147,11 +142,11 @@ $businessEnd->set_hook(
     "click",
     "talk",
     sub {
-    	my ($caller, $droid, $event, $data) = @_;
+        my ($caller, $droid, $event, $data) = @_;
 
-        $droid->ttsSpeak("Something nice");    	
-    	
-    	return;
+        $droid->ttsSpeak("Something nice");        
+        
+        return;
     }
 );
 
